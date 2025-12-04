7830dbe19f6acedbeede8415b085c1b4da9fe91c
lagesan <observer.lagesan@outlook.com>
2025-12-04 16:25:12 +0800

    modified:   G10.c

--- G10.c
+++ G10.c
@@ -1,77 +1,29 @@
 #include <kipr/wombat.h>
 
-const int LEFT_MOTOR = 3;
-const int RIGHT_MOTOR = 0;
-
-const int LINE_SENSOR = 0;
-const int FRONT_BUMPER = 7;
-const int REAR_BUMPER = 5;
-
-const int LINE_THRESHOLD = 3200; 
-
-const int FORWARD_SPEED = 90;
-const int TURN_SPEED = 50;
-
-const int TURN_180_MS = 1500;
-const int REAR_BUMPER_THRESHOLD = 1000;
-
 int main()
 {
-    printf("Ready for G10 Task (Analog Sensor Mode)\n");
-    printf("Waiting for start signal...\n");
-
-    msleep(2000);
+    int sensor_port = 0;
+    int left_motor = 3;
+    int right_motor = 0;
     
-    printf("Signal received, task starting!\n");
+    int threshold = 2000;
 
-    printf("Phase 1: Moving to finish line...\n");
-    while (digital(FRONT_BUMPER) == 0)
+    printf("Ready...\n");
+    msleep(3000);
+    
+    while(1)
     {
-        if (analog(LINE_SENSOR) > LINE_THRESHOLD)
+        if(analog(sensor_port) > threshold)
         {
-            motor(LEFT_MOTOR, FORWARD_SPEED);
-            motor(RIGHT_MOTOR, FORWARD_SPEED - TURN_SPEED);
+            motor(left_motor, 30);
+            motor(right_motor, 80);
         }
         else
         {
-            motor(LEFT_MOTOR, FORWARD_SPEED - TURN_SPEED);
-            motor(RIGHT_MOTOR, FORWARD_SPEED);
+            motor(left_motor, 100);
+            motor(right_motor, 30);
         }
-        msleep(10);
+        msleep(5);
     }
-    ao();
-    printf("Reached the finish line!\n");
-
-    printf("Phase 2: Preparing to return...\n");
-    motor(LEFT_MOTOR, -80);
-    motor(RIGHT_MOTOR, -80);
-    msleep(500);
-    ao();
-
-    motor(LEFT_MOTOR, 100);
-    motor(RIGHT_MOTOR, -100);
-    msleep(TURN_180_MS);
-    ao();
-    printf("Turned around, returning to start.\n");
-
-    printf("Phase 3: Returning to start area...\n");
-    while (analog(REAR_BUMPER) < REAR_BUMPER_THRESHOLD)
-    {
-        if (analog(LINE_SENSOR) > LINE_THRESHOLD)
-        {
-            motor(LEFT_MOTOR, FORWARD_SPEED);
-            motor(RIGHT_MOTOR, FORWARD_SPEED - TURN_SPEED);
-        }
-        else
-        {
-            motor(LEFT_MOTOR, FORWARD_SPEED - TURN_SPEED);
-            motor(RIGHT_MOTOR, FORWARD_SPEED);
-        }
-        msleep(10);
-    }
-    ao();
-    printf("Returned to the start area!\n");
-
-    printf("G10 Task Complete!\n");
     return 0;
 }
